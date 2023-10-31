e4c332f42135244b13646e5661dbbf28a519c94a
param-dandekar <param.dandekar@gmail.com>
2023-10-31 16:21:53 +0530

    Added basic boilerplate for app

--- bin/__init__.py
+++ bin/__init__.py
@@ -1,12 +1,16 @@
-import flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 DATABASE_NAME = "music_app.db"
 
 def make_app():
-    app = flask(__name__)
+    app = Flask(__name__)
     
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE_NAME}'
     db.init_app(app)
+
+
+
+    return app
 
